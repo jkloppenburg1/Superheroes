1c54c3e945805aa7e61a6a7260911ebc915b0445
jkloppenburg1 <jkloppenburg1@student.cccd.edu>
2016-10-09 00:32:13 -0700

    misc

--- app/src/main/java/com/example/nick/superheroes/QuizActivity.java
+++ app/src/main/java/com/example/nick/superheroes/QuizActivity.java
@@ -1,6 +1,11 @@
 package com.example.nick.superheroes;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +15,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class QuizActivity extends AppCompatActivity {
+    public static final String QUESTIONS = "pref_typeOfQuestions"; // What question will be asked
+
+    private boolean phoneDevice = true; // used to force portrait mode
+    private boolean preferencesChanged = true; // did preferences change?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,36 @@ public class QuizActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // set default values in SharedPreferences
+        PreferenceManager.getDefaultSharedPreferences(this).
+                registerOnSharedPreferenceChangeListener(
+                        preferencesChanged);
+
+        // register listener for SharedPreferences
+        PreferenceManager.getDefaultSharedPreferences(this).
+                registerOnSharedPreferenceChangeListener(
+                        preferencesChangeListener);
+
+        // determine screen size
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        // if tablet, set phoneDevice to false
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+        {
+            phoneDevice = false;
+        }
+
+        // if on a phone, only use portrait mode
+        if (phoneDevice)
+        {
+            setRequestedOrientation(
+                    ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+
+
+/*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,27 +65,67 @@ public class QuizActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
+    }
+
+    @Override
+    protected  void onStart()
+    {
+        super.onStart();
+
+        if (preferencesChanged)
+        {
+            // default preferences have been set,
+            // initialize QuizActivityFragment and start the quiz
+            QuizActivityFragment quizFragment = (QuizActivityFragment)
+                    getSupportFragmentManager().findFragmentById(
+                            R.id.quizFragment);
+            quizFragment.updateQuestion(
+                    PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.resetQuiz();
+            preferencesChanged = false;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+       int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            getMenuInflater().inflate(R.menu.menu_quiz, menu);
+            return true;
+        }
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
-        return true;
+        else
+            return false;
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
         return super.onOptionsItemSelected(item);
     }
+
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                    preferencesChanged = true; // user changed app setting
+
+                    QuizActivityFragment quizFragment = (QuizActivityFragment)
+                            getSupportFragmentManager().findFragmentById(
+                                    R.id.quizFragment);
+
+                    if (key.equals(QUESTIONS))
+                    {
+                        quizFragment.updateQuestion();
+                        quizFragment.resetQuiz();
+                    }
+
+                    Toast.makeText(QuizActivity.this, R.string.)
+                }
+            }
 }
